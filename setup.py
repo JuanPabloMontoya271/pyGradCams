99e54dcd712c0bf58cd8169e5478fcc99d86539a
JuanPabloMontoya <37849335+JuanPabloMontoya271@users.noreply.github.com>
2020-07-21 12:53:57 -0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,26 @@
+from distutils.core import setup
+setup(
+  name = 'pyGradCams',         
+  packages = ['pyGradCams'],   # Chose the same as "name"
+  version = '0.0',      
+  license='MIT',       
+  description = 'This is a python library to implement data structures and algorithms',   
+  author = 'Juan Pablo Montoya',                   
+  author_email = 'A01251887@itesm.mx',      
+  url = '',   
+  download_url = "",
+
+  keywords = ['python', 'data structures'],
+  install_requires=[           
+      ],
+  classifiers=[
+    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Intended Audience :: Developers',      # Define that your audience are developers
+    'Topic :: Software Development :: Build Tools',
+    'License :: OSI Approved :: MIT License',   # Again, pick a license
+    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'Programming Language :: Python :: 3.4',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+  ],
+)
